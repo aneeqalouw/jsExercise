98e0fec1b8fffed01edf4fc44379eceb8e349188
aneeqalouw <aneeqalouw057@gmail.com>
2023-11-09 08:44:42 +0200

    code

--- script/code.js
+++ script/code.js
@@ -1,5 +1,14 @@
-let num1 = document.querySelector('#num1')
-let num2 = document.querySelector('#num2')
-let btnCalc = document.querySelector('#btnAdd')
 
-let answer = num1 + num2
+let btnCalc = document.querySelector('#btnAdd')
+let output = document.querySelector('#output')
+
+
+function addition (){
+    let num1 = document.querySelector('#num1').value
+    let num2 = document.querySelector('#num2').value
+    let answer = +num1 + +num2;
+    output.innerHTML = answer;
+
+}
+
+btnCalc.addEventListener('click', addition);
